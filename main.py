41ec9bdbcd5d6f26138d9910c21d1ae88043e35c
leonh <leon.heilmann@gmx.de>
2022-01-24 16:59:02 +0100

    add main game loop

---
+++ main.py
@@ -0,0 +1,36 @@
+import requests
+
+
+def get_random(amount: int = 1):
+    r = requests.get(f"https://random-word-api.herokuapp.com/word?number={amount}")
+    return r.text.split("\"")[1]
+
+
+def run():
+    word = get_random()
+    placeholder = "_"
+    lives = 3
+    done = False
+    guess = placeholder * len(word)
+
+    while not done:
+        print("*"*30)
+        print(f"Current guess: {guess}\nWord: {word}\nLives: {lives}")
+        user_in = input("Enter your guess: ").lower()
+        if user_in in word:
+            for index, value in enumerate(word):
+                if value == user_in:
+                    s = list(guess)
+                    s[index] = user_in
+                    guess = "".join(s)
+        else:
+            lives -= 1
+
+        if lives <= 0 or word == guess:
+            done = True
+            print("*" * 30)
+            print(f"Game has ended\nThe word was {word}")
+
+
+if __name__ == '__main__':
+    run()
